b1cb63aeef1c3cd79b40816920bdbac3109cb25d
YU-K <xver456@gmail.com>
2021-04-13 13:47:41 +0400

    refactor Main

--- src/index.js
+++ src/index.js
@@ -2,52 +2,22 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import App from "./Components/App";
 import StateContext from "./context";
-import _ from "lodash";
 
 const Main = () => {
-  const [processState, updateProcessState] = useState("filling");
+  const [process, updateProcess] = useState("filling");
   const [addressObject, setAddress] = useState({});
-  const [userAddress, updateUserAddress] = useState({
-    country: "",
-    city: "",
-    postcode: "",
-    street: "",
-    housenumber: "",
-  });
-
-  const onSelect = (value) => {
-    if (!value) {
-      return;
-    }
-    const addressObj = value.properties;
-
-    setAddress({ ...addressObj });
-    updateProcessState("finished");
-  };
-
-  const getUserAddressObject = (obj1, obj2) => {
-    const keys = Object.keys(obj1);
-    keys.map((key) => {
-      if (!_.has(obj2, key)) {
-        return;
-      }
-      obj2[key] = obj1[key];
-    });
-    return;
-  };
-
-  const address = getUserAddressObject(addressObject, userAddress);
+  const [userAddress, updateUserAddress] = useState({});
 
   return (
     <React.StrictMode>
       <StateContext.Provider
         value={{
-          processState,
-          updateProcessState,
+          process,
+          updateProcess,
           userAddress,
           addressObject,
           updateUserAddress,
-          onSelect,
+          setAddress,
         }}
       >
         <App />
