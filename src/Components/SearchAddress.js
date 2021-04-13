6f6362e1f0a93063cc9632b484643cde77101360
YU-K <xver456@gmail.com>
2021-04-13 13:46:39 +0400

    refactor SearchAddress

--- src/Components/SearchAddress.js
+++ src/Components/SearchAddress.js
@@ -6,9 +6,47 @@ import "@geoapify/geocoder-autocomplete/styles/minimal.css";
 import { useContext } from "react";
 import StateContext from "../context";
 import Container from "@material-ui/core/Container";
+import _ from "lodash";
 
 const SearchAddress = () => {
-  const { onSelect } = useContext(StateContext);
+  const { updateProcess, setAddress, updateUserAddress } = useContext(
+    StateContext
+  );
+
+  const userAddressObject = {
+    state: "",
+    country: "",
+    city: "",
+    postcode: "",
+    street: "",
+    housenumber: "",
+  };
+
+  const getUserAddressObject = (obj1, obj2) => {
+    const keys = Object.keys(obj1);
+    keys.map((key) => {
+      if (!_.has(obj2, key)) {
+        return;
+      }
+      obj2[key] = obj1[key];
+    });
+    return;
+  };
+
+  const onSelect = (value) => {
+    if (!value) {
+      return;
+    }
+
+    const addressObj = value.properties;
+    getUserAddressObject(addressObj, userAddressObject);
+
+    setAddress({ ...addressObj });
+    updateProcess("selected");
+
+    updateUserAddress({ ...userAddressObject });
+  };
+
   return (
     <Container maxWidth="sm">
       <GeoapifyContext apiKey="06be80713b9640aea83b4571ec123be3">
